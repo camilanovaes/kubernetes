adf87a2bfabcd361ec7301b12e8c4e320b7bb476
Camila Novaes <novaes.csw@gmail.com>
2018-11-02 18:48:38 -0300

    Fix install script

--- scripts/install-kube.sh
+++ scripts/install-kube.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+# You have to run as ROOT
 
 echo "Installing Docker"
 
-sudo apt-get update
-sudo apt-get install \
+apt-get update
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+apt-get update
+apt-get install docker-ce
 
 echo "Installing Kubernetes"
 
